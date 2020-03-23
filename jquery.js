3b3a30da9d7a44d2475caec7ea5b0922df300cf9
eugenesinamban <49954579+eugenesinamban@users.noreply.github.com>
2020-03-23 12:16:02 +0900

    Initial commit

---
+++ jquery.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $("#burger2").click(function() {
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $("#burger2").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+  
+    });
+
+    $('#burger1').click(() => {
+        
+        $('#menu').toggleClass("is-hidden-mobile");
+        $("#burger1").toggleClass("is-active");
+
+    });
+
+    $('.card').click(function() {
+        var content = $(this).find('.card-content');
+        var footer = $(this).find('.card-footer');
+        $("svg", this).toggleClass("fa-angle-up fa-angle-down");
+        if (content.hasClass('active') || footer.hasClass('active')) {
+            
+            content.removeClass('active');
+            content.slideUp();
+            footer.removeClass('active');
+            footer.slideUp();
+
+        } else {
+
+            content.addClass('active');
+            content.slideDown();
+            footer.addClass('active');
+            footer.slideDown();
+
+        }
+        // $(this).find('.card-content').slideDown();
+        // $(this).find('.card .card-footer').slideDown();
+        
+    });
+
+  });   
